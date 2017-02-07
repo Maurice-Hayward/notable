a30f895008df182c4dd8827cefd09a35e2b63090
maurice_hayward <mauricehayward1@gmail.com>
2017-02-07 18:00:30 -0500

    finished the medium tutorial maybe add a UI later?

--- server.js
+++ server.js
@@ -3,15 +3,21 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-
+const db = require('./config/db.js');
 const PORT = 8000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+MongoClient.connect(db.url, (err,database)=>{
+	if (err) return console.error(err);
 
-require('./app/routes')(app, {});
-app.listen(PORT, ()=> {
+	require('./app/routes')(app, database);
+
+	app.listen(PORT, ()=> {
 	console.log("we are live on port "  + PORT);
 })
+
+})
+
